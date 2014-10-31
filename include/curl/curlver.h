e11ced4a28bf8bde5df8e471d32a0b9075924218
Scott M Anderson <smanders@sdl.usu.edu>
2014-10-31 09:33:29 -0600

    updating LIBCURL_VERSION, LIBCURL_TIMESTAMP
    * this wouldn't be needed if building from .tar.bz2
    * but since we're building from .zip (from github)...
    * because .tar.bz2 doesn't include all the necessary files (CMake/Macros.cmake)

--- include/curl/curlver.h
+++ include/curl/curlver.h
@@ -30,7 +30,7 @@
 
 /* This is the version number of the libcurl package from which this header
    file origins: */
-#define LIBCURL_VERSION "7.38.0-DEV"
+#define LIBCURL_VERSION "7.38.0"
 
 /* The numeric version number is also available "in parts" by using these
    defines: */
@@ -64,6 +64,6 @@
  *
  * "Mon Feb 12 11:35:33 UTC 2007"
  */
-#define LIBCURL_TIMESTAMP "DEV"
+#define LIBCURL_TIMESTAMP "Fri Oct 31 15:33:00 UTC 2014"
 
 #endif /* __CURL_CURLVER_H */
